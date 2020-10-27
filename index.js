f8777473aab9a92ad1aedd3594aeeb7af6d24656
thesebonesalone <duikhead2@gmail.com>
2020-10-26 19:06:26 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function wakeDog(name, breed) {
+    return `Wake ${name} the ${breed}`;
+}
+function leashDog(name, breed) {
+    return `Leash ${name} the ${breed}`;
+}
+function walkToPark(name, breed) {
+    return `Walk to the park with ${name} the ${breed}`;
+}
+function throwFrisbee(name, breed) {
+    return `Throw the frisbee for ${name} the ${breed}`;
+}
+function walkHome(name, breed) {
+    return `Walk home with ${name} the ${breed}`;
+}
+
+function unleashDog(name, breed) {
+    return `Unleash ${name} the ${breed}`;
+}
+
+function exerciseDog(name, breed) {
+    return routine.map(element => element(name,breed));
+}
